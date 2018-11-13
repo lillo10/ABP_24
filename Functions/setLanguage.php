ad5c37c584f178b3b72816fa215c597dba97f4c8
Angel Lillo Amaro <angel.lillo.amaro@gmail.com>
2018-11-13 16:24:45 +0100

    cfdsfcd

--- Functions/setLanguage.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	session_start();
-	$_SESSION['idioma'] = $_POST['idioma'];/*Post del idioma seleccionado*/
-	header('Location: '. $_SERVER["HTTP_REFERER"]);/*Redireccionamiento a de donde se viene*/
-?>
