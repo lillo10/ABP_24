1e862aecaa13c4090eea1e1a21b921ef385e82d0
franggomez <gofrango96@gmail.com>
2018-11-14 16:24:19 +0100

    Update Views/MenuLatIzq.php

--- Views/MenuLatIzq.php
+++ Views/MenuLatIzq.php
@@ -9,6 +9,10 @@ if(autenticado()){//Se debería usar un switch que de los permisos pero de momen
 	?>
 	<div class="menulateralizq" >
 			<div class="menudespleg">
+				<a href=''><button class="menu"><botonmenuizq>Pistas</botonmenuizq></button></a>
+			</div>
+			
+			<div class="menudespleg">
 				<a href=''><button class="menu"><botonmenuizq>Reservas</botonmenuizq></button></a>
 					<div class="opciones">
 						<a href="">Mis reservas</a>
