30b9697857dcd8e2aa78e7def9de84369ad126d2
franggomez <gofrango96@gmail.com>
2018-11-14 21:30:41 +0100

    Update Views/Pista/Pista_SHOWCURRENT.php

---
+++ Views/Pista/Pista_SHOWCURRENT.php
@@ -0,0 +1,50 @@
+<?php
+
+	
+class Pista_SHOWCURRENT{  
+	var $lista;
+	var $datos;
+	var $enlace;
+	
+	function __construct ($lista, $datos, $enlace){
+		$this -> lista = $lista;
+		$this -> datos = $datos;
+		$this -> enlace = $enlace;
+		$this->toString();
+	}
+
+	function toString(){		
+		include '../Views/Header.php';
+		include '../Views/MenuNavHorizontal.php';
+		include '../Views/MenuLatIzq.php';
+
+?>
+	
+		<form method="POST" accept-charset="UTF-8" id="formularioShowPista" name="formularioShowPista" style="display: inline-block;" action="../Controllers/Pista_CONTROLLER.php">			
+			<table id="tuplaDetail" border>
+				<tr>
+					<th colspan="2" > DATOS DE PISTA </th>
+				</tr>
+				<tr>
+					<th><?php echo $this->lista[0];?></th><td><?php echo $this -> datos[0]; ?></td>
+				</tr>
+				<tr>
+					<th><?php echo $this->lista[1];?></th><td><?php echo $this -> datos[1]; ?></td>
+				</tr>
+				<tr>
+					<th><?php echo $this->lista[2];?></th><td><?php echo $this -> datos[2]; ?></td>
+				</tr>
+				<tr>
+					<th><?php echo $this->lista[3];?></th><td><?php echo $this -> datos[3]; ?></td>
+				</tr>
+				<tr>
+					<th><?php echo 'Volver'; ?></th><td><a href="../Controllers/Pista_CONTROLLER.php"><img src="../img/return.png" height="27px"/></a></td>
+				</tr>
+			</table>
+		</form>
+		
+		</div>
+		<?php
+		include '../Views/Footer.php';
+	}
+} ?>
