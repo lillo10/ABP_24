19aabfcf568ad59e28f6d2fa149b457ce426a4de
franggomez <gofrango96@gmail.com>
2018-11-16 15:37:28 +0100

    Update Pista_DELETE.php

--- Views/Pista/Pista_DELETE.php
+++ Views/Pista/Pista_DELETE.php
@@ -25,26 +25,26 @@ class Pista_DELETE{
 					<th colspan="2" > DATOS DE PISTA A BORRAR </th>
 				</tr>
 				<tr>
-					<th><?php echo $this->lista[0];?></th><td><?php echo $this -> datos[0]; ?></td>
+					<th> idPista </th><td><?php echo $this -> datos[0]; ?></td>
 				</tr>
 				<tr>
-					<th><?php echo $this->lista[1];?></th><td><?php echo $this -> datos[1]; ?></td>
+					<th> Disponibilidad </th><td><?php echo $this -> datos[1]; ?></td>
 				</tr>
 				<tr>
-					<th><?php echo $this->lista[2];?></th><td><?php echo $this -> datos[2]; ?></td>
+					<th> Fecha </th><td><?php echo $this -> datos[2]; ?></td>
 				</tr>
 				<tr>
-					<th><?php echo $this->lista[3];?></th><td><?php echo $this -> datos[3]; ?></td>
+					<th> Hora </th><td><?php echo $this -> datos[3]; ?></td>
 				</tr>
 				<tr>
-					<th><?php echo 'Accion'; ?></th><td>
+					<th> Acción </th><td>
 						<input type="hidden" name="idPista" value="<?php echo $this -> datos[0]?>"/>
 						<input type="hidden" name="fecha" value="<?php echo $this -> datos[2]?>"/>
 						<input type="hidden" name="hora" value="<?php echo $this -> datos[3]?>"/>
 					<button onClick="submit" type="submit" name="orden" value="DELETE"/><img src="../img/erase.png" height="20px"/></td>
 				</tr>
 				<tr>
-					<th><?php echo 'Volver'; ?></th><td><a href="../Controllers/Pista_CONTROLLER.php"><img src="../img/return.png" height="27px"/></a></td>
+					<th> Volver </th><td><a href="../Controllers/Pista_CONTROLLER.php"><img src="../img/return.png" height="27px"/></a></td>
 				</tr>
 			</table>
 		</form>
