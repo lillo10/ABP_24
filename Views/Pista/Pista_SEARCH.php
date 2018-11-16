aac50bde3c58978980a05e8e2599f7e9396c8ee6
franggomez <gofrango96@gmail.com>
2018-11-16 11:23:41 +0100

    Update Pista_SEARCH.php

--- Views/Pista/Pista_SEARCH.php
+++ Views/Pista/Pista_SEARCH.php
@@ -22,7 +22,6 @@
 					<tri>
 						<tdi><?php echo $this->lista[0];?></tdi>
 						<tdi><input  type="text" id="<?php echo $this->lista[0];?>" name="<?php echo $this->lista[0];?>" size="5" maxlength="5"/> </tdi>
-						<tdi><img id="loginABot" height="20px" src="../img/red-button.png"/></tdi><tdi><texto-correccion id="loginABotText"></texto-correccion></tdi>
 					</tri>
 					<trp>
 						<tdp><?php echo $this->lista[1];?></tdp>
@@ -31,12 +30,10 @@
 							  <option value="SI"> SI </option>
 							  <option value="NO"> NO </option>
 						</select></tdp>
-						<tdp><img id="passwordABot" height="20px" src="../img/red-button.png"/></tdp><tdp><texto-correccion id="passwordABotText"></texto-correccion></tdp>
 					</trp>
 					<tri>
 						<tdi><?php echo $this->lista[2];?></tdi>
 						<tdi><input  type="date" id="<?php echo $this->lista[2];?>" name="<?php echo $this->lista[2];?>"  onkeydown="return false"/></tdi>
-						<tdi><img id="DNIABot" height="20px" src="../img/red-button.png"/></tdi><tdi><texto-correccion id="DNIABotText"></texto-correccion></tdi>
 					</tri>
 					<trp>
 						<tdp><?php echo $this->lista[3];?></tdp>
@@ -52,7 +49,6 @@
 							  <option value="19:30"> 19:30 </option>
 							  <option value="21:00"> 21:00 </option>
 						</select></tdp>
-						<tdp><img id="passwordABot" height="20px" src="../img/red-button.png"/></tdp><tdp><texto-correccion id="passwordABotText"></texto-correccion></tdp>
 					</trp>
 					<br/><br/>
 					<button type="submit" name="orden" value="SEARCH"/><img src="../img/search.png" height="30px"/>
