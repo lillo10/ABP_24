e66f7a162690e676a8387b7d47a730f49c2a321c
franggomez <gofrango96@gmail.com>
2018-11-21 00:16:30 +0100

    Update Pista_ADD.php

--- Views/Pista/Pista_ADD.php
+++ Views/Pista/Pista_ADD.php
@@ -18,23 +18,23 @@
 			<form method="POST" accept-charset="UTF-8" id="formularioAddPista" name="formularioAddPista" style="display: inline-block;" action="../Controllers/Pista_CONTROLLER.php">
 				<table class="formulario">
 					<tri>
-						<tdi> idPista </tdi>
-						<tdi><input required type="text" id="<?php echo $this->lista[0];?>" name="<?php echo $this->lista[0];?>" size="5" maxlength="5" /></tdi>
+						<tdi> Número de Pista </tdi>
+						<tdi><input required type="text" id="<?php echo $this->lista[1];?>" name="<?php echo $this->lista[1];?>" size="5" maxlength="5" /></tdi>
 					</tri>
 					<trp>
 						<tdp> Disponibilidad </tdp>
-						<tdp><select required id="<?php echo $this->lista[1];?>" name="<?php echo $this->lista[1];?>">
+						<tdp><select required id="<?php echo $this->lista[2];?>" name="<?php echo $this->lista[2];?>">
 							  <option value="SI"> SI </option>
 							  <option value="NO"> NO </option>
 						</select></tdp>
 					</trp>
 					<tri>
 						<tdi> Fecha </tdi>
-						<tdi><input required type="date" id="<?php echo $this->lista[2];?>" name="<?php echo $this->lista[2];?>"  onkeydown="return false" /></tdi>
+						<tdi><input required type="date" id="<?php echo $this->lista[3];?>" name="<?php echo $this->lista[3];?>"  onkeydown="return false" /></tdi>
 					</tri>
 					<trp>
 						<tdp> Hora </tdp>
-						<tdp><select required id="<?php echo $this->lista[3];?>" name="<?php echo $this->lista[3];?>">
+						<tdp><select required id="<?php echo $this->lista[4];?>" name="<?php echo $this->lista[4];?>">
 							  <option value="9:00"> 9:00 </option>
 							  <option value="10:30"> 10:30 </option>
 							  <option value="12:00"> 12:00 </option>
@@ -46,6 +46,10 @@
 							  <option value="21:00"> 21:00 </option>
 						</select></tdp>
 					</trp>
+					<tri>
+						<tdi> Precio </tdi>
+						<tdi><input required type="text" id="<?php echo $this->lista[5];?>" name="<?php echo $this->lista[5];?>" size="5" maxlength="5" /></tdi>
+					</tri>
 					<br/><br/>
 					<button type="submit" name="orden" value="ADD"/><img src="../img/add.png" height="30px"/>
 				</table>
