0b5fc78b9aad5e5bb71e286e7553623baef9b963
franggomez <gofrango96@gmail.com>
2018-11-21 00:17:06 +0100

    Create Reserva_SHOWCURRENT.php

---
+++ Views/Reserva/Reserva_SHOWCURRENT.php
@@ -0,0 +1,44 @@
+<?php
+	
+class Reserva_SHOWCURRENT{
+	
+	var $lista;
+		var $datos;
+		var $enlace;
+		
+		function __construct ($lista, $datos, $enlace){
+			$this -> lista = $lista;
+			$this -> datos = $datos;
+			$this -> enlace = $enlace;
+			$this->toString();
+	} 
+
+	function toString(){		
+		include '../Views/Header.php';
+		include '../Views/MenuNavHorizontal.php';
+		include '../Views/MenuLatIzq.php';
+?>
+	
+			
+			<table id="tuplaDetail" border>
+				<tr>
+					<th colspan="2" > DATOS DE RESERVA </th>
+				</tr>
+				<tr>
+					<th> idReserva </th><td><?php echo $this -> datos[0]; ?></td>
+				</tr>
+				<tr>
+					<th> Login </th><td><?php echo $this -> datos[1]; ?></td>
+				</tr>
+				<tr>
+					<th> Número de Pista </th><td><?php echo $this -> datos[2]; ?></td>
+				</tr>
+				<tr>
+					<th> Volver </th><td><a href="../Controllers/Reserva_CONTROLLER.php"><img src="../img/return.png" height="27px"/></a></td>
+				</tr>
+			</table>
+		</form>
+		</div><?php
+		include '../Views/Footer.php';
+	} 
+}
