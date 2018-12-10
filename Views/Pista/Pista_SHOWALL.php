75ec68272f03dfcde408616db2dab86b35968c36
franggomez <gofrango96@gmail.com>
2018-12-10 22:02:34 +0100

    Update Pista_SHOWALL.php

--- Views/Pista/Pista_SHOWALL.php
+++ Views/Pista/Pista_SHOWALL.php
@@ -17,16 +17,18 @@
 			include '../Views/MenuLatIzq.php';
 
 ?>
-		<section  id="showall">
-			<table>
-				<thead> 
-					<th colspan="12" > LISTA DE PISTAS 
+		<section  id="showall" >
+			<h1> LISTA DE PISTAS </h1>
+			 <table>
+				<!-- <thead> -->
+					 <th colspan="12" > BUSCAR PISTA
 						<form action="../Controllers/Pista_CONTROLLER.php" method="post" id="formularioShowallPista" name="formularioShowallPista">
 							<button onclick="document.forms[0].submit" name="orden" value="SEARCH"> <img src="../img/search.png" height="30px"/> </button> 
 							<!-- <button onClick="document.forms[0].submit" name="orden" value="ADD">  <img src="../img/add.png" height="30px"/> </button> -->
-						</form>
-					</th> 
-					<tr>
+						 </form>
+					</th>
+						</table> 
+					<!--<tr>
 						<?php
 							/*for($i = 0; $i < count($this -> lista)-1; $i++){
 								echo "<th>" . $this -> lista[$i] . "</th>";
@@ -38,12 +40,74 @@
 						<th> Fecha/Hora </th>
 						<th> Precio </th>
 						<th> Acciones </th>
-					</tr>
-				</thead>
-				<tbody> 
+					</tr>-->
+				<!-- </thead>
+				<tbody> -->
 					<?php
-					
+						$actual = "0";
+					$primera = true;
 					if(!is_string($this->datos)){
+						while($fila=$this->datos->fetch_row()){ 
+							if(strcmp($fila[1], $actual) == 0){
+								echo "<tr>";
+								for($col=0; $col<count($fila); $col++){ 
+									echo "<td>" . $fila[$col] . "</td>";	
+								} ?>
+								<td> 
+								<form action="../Controllers/Pista_CONTROLLER.php" method="post" name="showall">
+								<input type="hidden" id="idPista" name="idPista" value= "<?php echo $fila[0]; ?>" />
+									<!-- <input type="hidden" id="numPista" name="numPista" value= "<?php //echo $fila[1]; ?>" />
+									<input type="hidden" id="fecha" name="fecha" value= "<?php //echo $fila[3]; ?>" />
+									<!-- <input type="hidden" id="hora" name="hora" value= <?php //echo $fila[4]; ?> /> -->
+									
+									<button onclick="document.forms[1].submit" name="orden" value="DELETE1">  <img src="../img/erase.png" height="30px"/> </i> </button> 
+									<button onclick="document.forms[1].submit" name="orden" value="EDIT">  <img src="../img/edit.png" height="30px"/> </button>  
+									<button onclick="document.forms[1].submit" name="orden" value="SHOWCURRENT">  <img src="../img/detail.png" height="30px"/> </button>
+								</form>
+							</td>
+								<?php
+								echo "</tr>";
+							}else{
+								if($primera){
+									$primera = false;
+								}else{
+									echo "</table>";
+								}
+								?>
+								<table>
+								<th colspan="12" > PISTA <?php echo $fila[1]?> </th>
+								<tr>
+									<th> idPista </th>
+									<th> Número de Pista </th>
+									<th> Disponibilidad</th>
+									<th> Fecha/Hora </th>
+									<th> Precio </th>
+									<th> Acciones </th>
+								</tr>  
+								<?php
+								echo "<tr>";
+								for($col=0; $col<count($fila); $col++){ 
+									echo "<td>" . $fila[$col] . "</td>";
+								} ?>
+								<td> 
+								<form action="../Controllers/Pista_CONTROLLER.php" method="post" name="showall">
+								<input type="hidden" id="idPista" name="idPista" value= "<?php echo $fila[0]; ?>" />
+									<!-- <input type="hidden" id="numPista" name="numPista" value= "<?php //echo $fila[1]; ?>" />
+									<input type="hidden" id="fecha" name="fecha" value= "<?php //echo $fila[3]; ?>" />
+									<!-- <input type="hidden" id="hora" name="hora" value= <?php //echo $fila[4]; ?> /> -->
+									
+									<button onclick="document.forms[1].submit" name="orden" value="DELETE1">  <img src="../img/erase.png" height="30px"/> </i> </button> 
+									<button onclick="document.forms[1].submit" name="orden" value="EDIT">  <img src="../img/edit.png" height="30px"/> </button>  
+									<button onclick="document.forms[1].submit" name="orden" value="SHOWCURRENT">  <img src="../img/detail.png" height="30px"/> </button>
+								</form>
+							</td>
+								<?php
+								echo "</tr>";
+							}
+							$actual = $fila[1];
+						}
+					}
+					/*if(!is_string($this->datos)){
 						while($fila=$this->datos->fetch_row()){
 							echo "<tr>" ;
 							for($col=0; $col<count($fila); $col++){
@@ -61,10 +125,9 @@
 							</tr>
 						<?php 
 						}
-					}?>
-				</tbody>
-			</table>
-			<br><br> <a href='../Controllers/Index_CONTROLLER.php'> Volver </a> 
+					}*/?>
+				<!-- </tbody>
+			</table> -->
 		</section>
 
 <?php
