6333ee518fbe7fde75faeac7c217a966046884ec
franggomez <gofrango96@gmail.com>
2018-12-26 11:51:18 +0100

    Update Anotarse_Campeonato.php

--- Controllers/Anotarse_Campeonato.php
+++ Controllers/Anotarse_Campeonato.php
@@ -7,7 +7,7 @@ include '../Views/Campeonato/Campeonato_SHOWALL.php';
 include '../Models/CAMPEONATO.php';
 
 if(autenticado()){/*Si está autenticado*/
-	echo $_GET['idCampeonato'];
+	//echo $_GET['idCampeonato'];
 	$campeonato = new Campeonato($_GET['idCampeonato'],'','','','');
 	$respuesta = $campeonato->SHOWALL_INSCRIPCION();
 	new Campeonato_SHOWALL($respuesta, '');
