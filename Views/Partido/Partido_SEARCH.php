cec7a19b9209d8478555b4beaf2d410aba6a754a
MazaJunior <44473098+MazaJunior@users.noreply.github.com>
2019-01-01 21:02:27 +0100

    Update Partido_SEARCH.php

--- Views/Partido/Partido_SEARCH.php
+++ Views/Partido/Partido_SEARCH.php
@@ -20,27 +20,24 @@
 			<form method="POST" accept-charset="UTF-8" id="formularioSearchPartido" name="formularioSearchPartido" style="display: inline-block;" action="../Controllers/Partido_CONTROLLER.php">
 				<table class="formulario">
 					<tri>
-						<tdi><?php echo $this->lista[0];?></tdi>
+						<tdi>Numero de Pista</tdi>
 						<tdi><input  type="text" id="<?php echo $this->lista[0];?>" name="<?php echo $this->lista[0];?>" size="5" maxlength="5"/> </tdi>
-						<tdi><img id="loginABot" height="20px" src="../img/red-button.png"/></tdi><tdi><texto-correccion id="loginABotText"></texto-correccion></tdi>
 					</tri>
 					<trp>
-						<tdp><?php echo $this->lista[1];?></tdp>
+						<tdp>Jugadores</tdp>
 						<tdp><select id="<?php echo $this->lista[1];?>" name="<?php echo $this->lista[1];?>">
 							  <option value="0"> 0 </option>
 							  <option value="1"> 1 </option>
 							  <option value="2"> 2 </option>
 							  <option value="3"> 3 </option>
 						</select></tdp>
-						<tdp><img id="passwordABot" height="20px" src="../img/red-button.png"/></tdp><tdp><texto-correccion id="passwordABotText"></texto-correccion></tdp>
 					</trp>
 					<tri>
-						<tdi><?php echo $this->lista[2];?></tdi>
+						<tdi>Fecha</tdi>
 						<tdi><input  type="date" id="<?php echo $this->lista[2];?>" name="<?php echo $this->lista[2];?>"  onkeydown="return false"/></tdi>
-						<tdi><img id="DNIABot" height="20px" src="../img/red-button.png"/></tdi><tdi><texto-correccion id="DNIABotText"></texto-correccion></tdi>
 					</tri>
 					<trp>
-						<tdp><?php echo $this->lista[3];?></tdp>
+						<tdp>Hora</tdp>
 						<tdp><select  id="<?php echo $this->lista[3];?>" name="<?php echo $this->lista[3];?>">
 							  <option value=""> -- </option>
 							  <option value="9:00"> 9:00 </option>
@@ -53,7 +50,6 @@
 							  <option value="19:30"> 19:30 </option>
 							  <option value="21:00"> 21:00 </option>
 						</select></tdp>
-						<tdp><img id="passwordABot" height="20px" src="../img/red-button.png"/></tdp><tdp><texto-correccion id="passwordABotText"></texto-correccion></tdp>
 					</trp>
 					<br/><br/>
 					<button type="submit" name="action" value="search"/><img src="../img/search.png" height="30px"/>
